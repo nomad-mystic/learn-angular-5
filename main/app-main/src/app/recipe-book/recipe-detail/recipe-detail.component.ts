3380280739379ecd70067e79210e66bfa285db58
Keith Murphy <nomadmystics@gmail.com>
2018-03-22 22:44:04 -0700

    LA-8: Finished up section 3 of the course. Moving on to next section.

--- main/app-main/src/app/recipe-book/recipe-detail/recipe-detail.component.ts
+++ main/app-main/src/app/recipe-book/recipe-detail/recipe-detail.component.ts
@@ -5,6 +5,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.css']
 })
+
+
 export class RecipeDetailComponent implements OnInit {
 
   constructor() { }
